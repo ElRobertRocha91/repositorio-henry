0ea8c20278f16dd3e49bc389478345ae8c8c8715
elrobertrocha91 <robertoestebanrocha91@gmail.com>
2022-09-18 15:43:03 -0300

    Complete M1 exercise

--- FT-M1-master/01-IntroToCS/homework/homework.js
+++ FT-M1-master/01-IntroToCS/homework/homework.js
@@ -2,11 +2,27 @@
 
 function BinarioADecimal(num) {
   // tu codigo aca
+  let array = num.split("").reverse().join("");
+  let suma = 0;
   
+  for (let i = 0; i < array.length; i++){
+    suma += array[i] * 2 ** i;
+  }
+  return suma;
+
 }
 
 function DecimalABinario(num) {
   // tu codigo aca
+  let numBinario = [];
+
+  while(num !== 0){
+    let numDivisor = Math.floor(num/2);
+    let resto = (num % 2).toString();
+    numBinario.push(resto);
+    num = numDivisor;
+  }
+  return numBinario.reverse().join("");
 
 }
 
