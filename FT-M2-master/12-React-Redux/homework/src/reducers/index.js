2ce3cb80ef0a1783e58a686a3c94127a1ccd8120
elrobertrocha91 <robertoestebanrocha91@gmail.com>
2023-04-05 23:28:20 -0300

    Netflix

---
+++ FT-M2-master/12-React-Redux/homework/src/reducers/index.js
@@ -0,0 +1,37 @@
+import { GET_MOVIES, GET_DETAIL, ADD_FAVORITE, REMOVE_FAVORITE } from "../actions";
+
+const initialState = {
+    moviesFavourites: [],
+    moviesLoaded: [],
+    movieDetail:{}
+};
+
+function rootReducer(state = initialState, action){
+    if(action.type === GET_MOVIES){
+        return {
+            ...state,
+            moviesLoaded: action.payload.Search
+        }
+    }
+    if(action.type === GET_DETAIL){
+        return {
+            ...state,
+            movieDetail: action.payload
+        }
+    }
+    if(action.type === ADD_FAVORITE){
+        return {
+            ...state,
+            moviesFavourites: state.moviesFavourites.concat(action.payload)
+        }
+    }
+    if(action.type === REMOVE_FAVORITE){
+        return {
+            ...state,
+            moviesFavourites: state.moviesFavourites.filter(movie => movie.imdbID !== action.payload)
+        }
+    }
+    return state;
+}
+
+export default rootReducer;
