59045607f37bf9dc2de1eb8f1347272f186dea92
elrobertrocha91 <robertoestebanrocha91@gmail.com>
2023-04-06 20:58:15 -0300

    Netflix

--- FT-M2-master/12-React-Redux/homework/src/actions/index.js
+++ FT-M2-master/12-React-Redux/homework/src/actions/index.js
@@ -4,11 +4,12 @@ export const ADD_FAVORITE = "ADD_FAVORITE";
 export const REMOVE_FAVORITE = "REMOVE_FAVORITE";
 
 const API = "http://www.omdbapi.com/?i=tt3896198&apikey=cf2f0a4d"
-const API_ID = "http://www.omdbapi.com/?apikey=cf2f0a4d"
+//const API_ID = "http://www.omdbapi.com/?apikey=cf2f0a4d"
+const APY_KEY = "cf2f0a4d"
 
 export function getMovies(name){
-    return async function(dispatch){
-        return await fetch(`${API}&s=${name}`)
+    return function(dispatch){
+        return fetch(`${API}&s=${name}`)
         .then(response => response.json())
         .then(json => {
             return dispatch({type: GET_MOVIES, payload: json});
@@ -17,8 +18,8 @@ export function getMovies(name){
 }
 
 export function getDetail(id){
-    return async function(dispatch){
-        return await fetch(`${API_ID}&i=${id}`)
+    return function(dispatch){
+        return fetch(`http://www.omdbapi.com/?apikey=${APY_KEY}&i=${id}`)
         .then(response => response.json())
         .then(json => {
             dispatch({type: GET_DETAIL, payload: json})
@@ -33,9 +34,9 @@ export function addMovieFavorite(payload){
     }
 }
 
-export function removeMovieFavorite(payload){
+export function removeMovieFavorite(id){
     return {
         type: REMOVE_FAVORITE,
-        payload
+        payload: id
     }
 }
