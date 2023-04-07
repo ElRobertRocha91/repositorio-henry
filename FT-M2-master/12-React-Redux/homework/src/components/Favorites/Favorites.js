a331adf3c87e0de23188693667a598e1650e8cc3
elrobertrocha91 <robertoestebanrocha91@gmail.com>
2023-04-06 21:05:56 -0300

    Netflix

--- FT-M2-master/12-React-Redux/homework/src/components/Favorites/Favorites.js
+++ FT-M2-master/12-React-Redux/homework/src/components/Favorites/Favorites.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
-// import { connect } from "react-redux";
-// import { Link } from 'react-router-dom';
+import { connect } from "react-redux";
+import { Link } from 'react-router-dom';
+import { removeMovieFavorite } from "../../actions";
 import './Favorites.css';
 
 export class ConnectedList extends Component {
@@ -11,12 +12,26 @@ export class ConnectedList extends Component {
         <h2>Películas Favoritas</h2>
         <ul>
           {/* Aqui deberias poner tu lista de peliculas! */}
+          {
+            this.props.favorites && this.props.favorites.map(movie =>
+              <div key={movie.id}>
+                <Link to={`/movie/${movie.id}`}>{movie.title}</Link>
+                <button onClick={() => this.props.removeMovieFavorite(movie.id)}>REMOVE FROM FAVORITES</button>
+                {/* <button onClick={() => this.props.dispatch({type: REMOVE_FAVORITE, id: movie.id})}>REMOVE FROM FAVORITES</button> */}
+              </div>
+              )
+          }
         </ul>
       </div>
     );
   }
 }
 
+function mapStateToProps(state){
+  return {
+    favorites: state.moviesFavourites
+  }
+}
 
 
-export default (ConnectedList);
+export default connect(mapStateToProps, {removeMovieFavorite}) (ConnectedList);
