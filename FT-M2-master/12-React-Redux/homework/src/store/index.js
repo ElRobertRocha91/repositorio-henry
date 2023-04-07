edfaec68566990495a643d5d9fe3993a6090a735
elrobertrocha91 <robertoestebanrocha91@gmail.com>
2023-04-06 21:01:14 -0300

    Netflix

--- FT-M2-master/12-React-Redux/homework/src/store/index.js
+++ FT-M2-master/12-React-Redux/homework/src/store/index.js
@@ -2,9 +2,12 @@ import { createStore, applyMiddleware, compose } from "redux";
 import rootReducer from "../reducers/index";
 import thunk from "redux-thunk";
 
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
 const store = createStore(
     rootReducer,
-    applyMiddleware(thunk)
+    composeEnhancer(applyMiddleware(thunk))
+    //applyMiddleware(thunk)
 );
 
 export default store;
