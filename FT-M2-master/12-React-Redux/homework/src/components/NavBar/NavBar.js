975c11550cb57a5b2c5eba4a2b00de52ba47c15e
elrobertrocha91 <robertoestebanrocha91@gmail.com>
2023-04-08 17:44:31 -0300

    Estilos Netflix

--- FT-M2-master/12-React-Redux/homework/src/components/NavBar/NavBar.js
+++ FT-M2-master/12-React-Redux/homework/src/components/NavBar/NavBar.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import Logo from '../../logoHenry.png'
+//import Logo from '../../logoHenry.png'
 
 import './Navbar.css';
 
 export default function NavBar() {
     return (
         <header className="navbar">
-            <div>
-                <img id="logoHenry" src={Logo} width="30" height="30" className="d-inline-block align-top" alt="" />
-            </div>
+            <div className='contenedor'>
+                <h2 className="logotipo">NETFLIX</h2>
+                 {/* <img id="logoHenry" src={Logo} width="30" height="30" className="d-inline-block align-top" alt="" /> */}
             <nav>
                 <ul className="list">
                     <li className="list-item">
@@ -18,6 +18,7 @@ export default function NavBar() {
                     </li>
                 </ul>
             </nav>
+            </div>
         </header>
     )
 }
